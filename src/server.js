078076a210f9c9f2053176241a0ef3e4ff8ffd3b
Max Granberg <mg224em@student.lnu.se>
2024-04-04 14:34:11 +0200

    Add a server.js file

---
+++ src/server.js
@@ -0,0 +1,79 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Max Granberg
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import logger from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec from './config/swaggerConfig.js'
+import { router } from './routes/router.js'
+import { connectDB } from './config/mongoose.js'
+
+try {
+  await connectDB()
+
+  const app = express()
+
+  // Set various HTTP headers to make the application little more secure (https://www.npmjs.com/package/helmet).
+  app.use(helmet())
+
+  // Enable CORS for the server
+  app.use(cors({ origin: ['https://fflapi-a68806964222.herokuapp.com/', 'http://localhost:8080'], credentials: true }))
+
+  // Set up a morgan logger using the dev format for log entries.
+  app.use(logger('dev'))
+
+  // Parse requests of the content type application/json.
+  app.use(express.json())
+
+  // Swagger setup
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+  // Register routes.
+  app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause
+          ? {
+              status: err.cause.status,
+              message: err.cause.message,
+              stack: err.cause.stack
+            }
+          : null,
+        stack: err.stack
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
